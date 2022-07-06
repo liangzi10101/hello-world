f861dc73671adb20f65b69ffb2aa34ca80db9699
梁子 <liangzi.101@bytedance.com>
2022-07-06 21:07:25 +0800

    feat: changchang

--- src/base.cpp
+++ src/base.cpp
@@ -1,14 +1,11 @@
 #include "base.h"
 #include <iostream>
 
-base::base()
-{
-}
+base::base() {}
 
-base::~base()
-{
-}
+base::~base() {}
 
-void myFunc(std::vector<int> set0, std::vector<int> set1, std::vector<int> set07, std::vector<int> set2, std::vector<int> set3, std::vector<int> set4, std::vector<int> set5, std::vector<int> set6) {
-    std::cout <<"haha" << std::endl;
+void myFunc(std::vector<int> set0, std::vector<int> set1, std::vector<int> set07, std::vector<int> set2, std::vector<int> set3,
+            std::vector<int> set4, std::vector<int> set5, std::vector<int> set6) {
+    std::cout << "hahads" << std::endl;
 }
